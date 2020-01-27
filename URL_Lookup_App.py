3324778f91dade4f8440963084a40683d1cff695
djnachovega <60361501+djnachovega@users.noreply.github.com>
2020-01-27 10:34:18 -0800

    Add files via upload

---
+++ URL_Lookup_App.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+import cgitb
+import cgi
+from URL_Lookup_Service.URL_Lookup_Service import URL_Lookup_Service
+import json
+
+"""
+CGI flag for debugging purpose
+True = execute python through html
+False = execute python directly
+"""
+CGI = True
+MalwareURL = True
+
+"""
+CGI stuff to connect from HTML
+"""
+cgitb.enable()
+print "Content-Type: text/plain;charset=utf-8"
+print
+form = cgi.FieldStorage()
+
+if CGI:
+    URL = form["Field"].value
+else:
+    URL = "bolo100.com"
+
+LookupInst = URL_Lookup_Service(URL)
+
+try:
+    MalwareURL = LookupInst.lookup()
+except:
+    print "lookup Error"
+    raise
+
+response = {}
+if MalwareURL:
+    response[URL] = "Malware"
+    # print "URL is Malware (Not Safe)"
+else:
+    response[URL] = "Safe"
+    # print "URL is Safe"
+print json.dumps(response)
